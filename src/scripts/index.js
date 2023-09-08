c9d51c517b1d2d91b4f68bc6a77a3a615c393a38
João Gomes <86986385+joaoeduardogomes@users.noreply.github.com>
2023-09-08 18:26:07 -0300

    content edition seems to work

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,16 @@
+function createNewLi(ul) {
+    const newLi = document.createElement('li');
+    ul.appendChild(newLi);
+}
+
 const ulItems = document.querySelectorAll('ul');
 
 ulItems.forEach((ulItem) => {
     ulItem.contentEditable = true;
+
+    ulItem.addEventListener('click', () => {
+        if (ulItem.innerHTML.length === 0) {
+            createNewLi(ulItem);
+        }
+    })
 })
